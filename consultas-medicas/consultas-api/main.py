2a413e82ec5be95cd7fbc582e8e6461427b6df9e
Wendels1 <wende.ferreira@iesb.edu.br>
2025-10-24 22:04:49 -0300

    Comenta criacao de DB para deploy

--- consultas-medicas/consultas-api/main.py
+++ consultas-medicas/consultas-api/main.py
@@ -1,17 +1,17 @@
 from fastapi import FastAPI
 from app.database.connection import engine, Base
 from app.models import user
-from app.models import appointment_model # <-- Novo modelo
+from app.models import appointment_model 
 from app.routes import user_routes 
 from app.routes import auth_routes 
-from app.routes import appointment_routes # <-- Novas rotas
+from app.routes import appointment_routes 
 
 # Função para criar a tabela 'users' e 'appointments'
-def create_db_tables():
+#def create_db_tables():
     # Isso cria TODAS as tabelas que herdam de Base, incluindo a nova 'appointments'
-    Base.metadata.create_all(bind=engine)
+    #Base.metadata.create_all(bind=engine)
 
-create_db_tables() 
+#create_db_tables() 
 
 app = FastAPI(title="Consultas Médicas API")
 
